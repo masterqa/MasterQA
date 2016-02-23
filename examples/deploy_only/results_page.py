2699b33a400bf626fb1cfe2c7d19bd04bdd89fd6
Michael Mintz <mdmintz@gmail.com>
2016-02-23 14:34:36 -0500

    Update the deploy test

--- examples/deploy_only/results_page.py
+++ examples/deploy_only/results_page.py
@@ -6,4 +6,4 @@ class MasterQATests(MasterQA):
     def test_results_page_only(self):
         ''' Importing MasterQA should take you to the results
         page at the end of the test, even if testing nothing. '''
-        pass
+        self.auto_close_results()
