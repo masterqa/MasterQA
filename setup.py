3f2ce1fcb167474bafc4eaa17609e8a8a65824a7
Michael Mintz <mdmintz@gmail.com>
2016-02-23 11:29:40 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""
+The setup package to install MasterQA requirements
+"""
+
+from setuptools import setup, find_packages  # noqa
+
+setup(
+    name='masterqa',
+    version='1.0.1',
+    url='https://github.com/mdmintz/MasterQA',
+    author='Michael Mintz',
+    author_email='@mintzworld',
+    maintainer='Michael Mintz',
+    description='Acceptance Testing Tool: https://github.com/mdmintz/MasterQA',
+    license='The MIT License',
+    install_requires=[
+        'seleniumbase==1.1.34',
+        'flake8==2.5.4',
+        ],
+    packages=['masterqa'],
+    entry_points={
+        'nose.plugins': []
+        }
+    )
