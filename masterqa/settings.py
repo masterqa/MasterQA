a01e326854359572a0b6a00e63632702dc7ac68a
Michael Mintz <mdmintz@gmail.com>
2016-02-25 04:13:08 -0500

    Add the maximum idle time to settings.py

--- masterqa/settings.py
+++ masterqa/settings.py
@@ -2,13 +2,16 @@
 MasterQA Settings
 
 You'll probably want to customize this to your own environment and needs.
-
-NOSETESTS USERS: IF YOU MAKE CHANGES TO THIS FILE, YOU NEED TO RERUN
-``python setup.py install`` IN ORDER FOR YOUR CHANGES TO TAKE EFFECT.
 """
 
+# The default message that appears when you don't specify a custom message
 DEFAULT_VALIDATION_MESSAGE = "Does the page look good?"
 
+# The time delay (in seconds) before the validation pop-up appears
 WAIT_TIME_BEFORE_VERIFY = 1.0
 
+# If True, the automation will start in full-screen mode
 START_IN_FULL_SCREEN_MODE = True
+
+# The maximimum idle time allowed (in seconds) before timing out and exiting
+MAX_IDLE_TIME_BEFORE_QUIT = 600
