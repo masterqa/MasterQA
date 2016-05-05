421b0b49970b5574e982c73839f593462c33f412
Michael Mintz <mdmintz@gmail.com>
2016-05-04 21:32:06 -0400

    Version 1.0.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages  # noqa
 
 setup(
     name='masterqa',
-    version='1.0.6',
+    version='1.0.7',
     url='http://masterqa.com',
     author='Michael Mintz',
     author_email='@mintzworld',
