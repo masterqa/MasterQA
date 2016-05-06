3ad45a177b8e680116eeb75ad4579257dcf770b2
Michael Mintz <mdmintz@gmail.com>
2016-05-06 15:59:10 -0400

    Font size

--- masterqa/master_qa.py
+++ masterqa/master_qa.py
@@ -256,7 +256,7 @@ class __MasterQATestCase__(BaseCase):
                 line = line.replace('"', '')
                 new_view_3 += '<tr><td>%s</tr>\n' % line
         new_view_3 += '</tbody></table>'
-        new_view_4 = '''<h3 style="color:#0C8CDB; font-size:27px;">
+        new_view_4 = '''<h3 style="color:#0C8CDB; font-size:24px;">
             MasterQA is powered by SeleniumBase</h3>'''
         new_view = '%s%s%s%s' % (
             new_view_1, new_view_2, new_view_3, new_view_4)
