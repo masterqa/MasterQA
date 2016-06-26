de63a077472ae6120eaba3f2239c0fe861f997a4
Michael Mintz <mdmintz@gmail.com>
2016-06-25 21:40:52 -0400

    Use seleniumbase 1.1.60

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='Automation-Assisted Manual Testing - http://masterqa.com',
     license='The MIT License',
     install_requires=[
-        'seleniumbase==1.1.57',
+        'seleniumbase==1.1.60',
         'flake8==2.5.4',
         ],
     packages=['masterqa'],
