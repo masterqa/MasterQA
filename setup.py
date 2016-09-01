b78deadd979179cae0d7d78c6ca577c36d2ba83d
Michael Mintz <mdmintz@gmail.com>
2016-09-01 01:49:38 -0400

    masterqa flake8 version should match seleniumbase

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license='The MIT License',
     install_requires=[
         'seleniumbase==1.2.8',
-        'flake8==2.6.2',
+        'flake8==3.0.4',
         ],
     packages=['masterqa'],
     entry_points={
