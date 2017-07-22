cd94531ac8d1e7025acac672248a81946ab3605f
Michael Mintz <mdmintz@gmail.com>
2017-07-22 19:27:58 -0400

    Version 1.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages  # noqa
 
 setup(
     name='masterqa',
-    version='1.0.28',
+    version='1.1.0',
     description='Automation-Assisted Manual Testing - http://masterqa.com',
     long_description='Automation-Assisted Manual Testing. http://masterqa.com',
     platforms='Mac * Windows * Linux',
