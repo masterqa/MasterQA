144fd964a1f7f859d9570f9e69d884ba9c042e4e
Michael Mintz <mdmintz@gmail.com>
2017-07-22 18:04:21 -0400

    Update requirements and details

--- setup.py
+++ setup.py
@@ -7,15 +7,17 @@ from setuptools import setup, find_packages  # noqa
 setup(
     name='masterqa',
     version='1.0.28',
+    description='Automation-Assisted Manual Testing - http://masterqa.com',
+    long_description='Automation-Assisted Manual Testing. http://masterqa.com',
+    platforms='Mac * Windows * Linux',
     url='http://masterqa.com',
     author='Michael Mintz',
-    author_email='@mintzworld',
+    author_email='mdmintz@gmail.com',
     maintainer='Michael Mintz',
-    description='Automation-Assisted Manual Testing - http://masterqa.com',
     license='The MIT License',
     install_requires=[
-        'seleniumbase>=1.3.22',
-        'flake8==3.3.0',
+        'seleniumbase>=1.4.2',
+        'flake8>=3.3.0',
         ],
     packages=['masterqa'],
     entry_points={
