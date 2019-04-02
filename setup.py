bc1aa05c054ccbea742c8268489dd5c5cc1d3139
Michael Mintz <mdmintz@gmail.com>
2019-04-02 13:52:56 -0400

    Update pypi description of MasterQA

--- setup.py
+++ setup.py
@@ -3,12 +3,23 @@ The setup package to install MasterQA requirements
 """
 
 from setuptools import setup, find_packages  # noqa
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+long_description = None
+try:
+    with open(path.join(this_directory, 'README.md'), 'rb') as f:
+        long_description = f.read().decode('utf-8')
+except IOError:
+    long_description = (
+        'Automation-Assisted Manual Testing - http://masterqa.com')
 
 setup(
     name='masterqa',
     version='1.1.4',
     description='Automation-Assisted Manual Testing - http://masterqa.com',
-    long_description='Automation-Assisted Manual Testing. http://masterqa.com',
+    long_description=long_description,
     platforms='Mac * Windows * Linux',
     url='http://masterqa.com',
     author='Michael Mintz',
