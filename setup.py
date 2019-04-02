2d08726b58d719ba6c290024f5fb3fde88f9911b
Michael Mintz <mdmintz@gmail.com>
2019-04-02 16:25:43 -0400

    Version 1.1.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except IOError:
 
 setup(
     name='masterqa',
-    version='1.1.5',
+    version='1.1.6',
     description='Automation-Assisted Manual Testing - http://masterqa.com',
     long_description=long_description,
     long_description_content_type='text/markdown',
