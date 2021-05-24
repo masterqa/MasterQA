e34c463b117e21b810ce0554bc39b009c1b6a484
Michael Mintz <mdmintz@gmail.com>
2021-05-24 16:32:15 -0400

    Refresh MasterQA

--- setup.py
+++ setup.py
@@ -3,32 +3,70 @@ The setup package to install MasterQA requirements
 """
 
 from setuptools import setup, find_packages  # noqa
-from os import path
+import os
+import sys
 
 
-this_directory = path.abspath(path.dirname(__file__))
+this_directory = os.path.abspath(os.path.dirname(__file__))
 long_description = None
+total_description = None
 try:
-    with open(path.join(this_directory, 'README.md'), 'rb') as f:
-        long_description = f.read().decode('utf-8')
+    with open(os.path.join(this_directory, 'README.md'), 'rb') as f:
+        total_description = f.read().decode('utf-8')
+    description_lines = total_description.split('\n')
+    long_description_lines = []
+    for line in description_lines:
+        if not line.startswith("<meta ") and not line.startswith("<link "):
+            long_description_lines.append(line)
+    long_description = "\n".join(long_description_lines)
 except IOError:
     long_description = (
         'Automation-Assisted Manual Testing - http://masterqa.com')
 
+if sys.argv[-1] == 'publish':
+    reply = None
+    input_method = input
+    if not sys.version_info[0] >= 3:
+        input_method = raw_input  # noqa
+    reply = str(input_method(
+        '>>> Confirm release PUBLISH to PyPI? (yes/no): ')).lower().strip()
+    if reply == 'yes':
+        print("\n*** Checking code health with flake8:\n")
+        os.system("python -m pip install 'flake8==3.9.0'")
+        flake8_status = os.system("flake8 --exclude=temp")
+        if flake8_status != 0:
+            print("\nWARNING! Fix flake8 issues before publishing to PyPI!\n")
+            sys.exit()
+        else:
+            print("*** No flake8 issues detected. Continuing...")
+        print("\n*** Rebuilding distribution packages: ***\n")
+        os.system('rm -f dist/*.egg; rm -f dist/*.tar.gz; rm -f dist/*.whl')
+        os.system('python setup.py sdist bdist_wheel')  # Create new tar/wheel
+        print("\n*** Installing twine: *** (Required for PyPI uploads)\n")
+        os.system("python -m pip install 'twine>=1.15.0'")
+        print("\n*** Installing tqdm: *** (Required for PyPI uploads)\n")
+        os.system("python -m pip install --upgrade 'tqdm>=4.61.0'")
+        print("\n*** Publishing The Release to PyPI: ***\n")
+        os.system('python -m twine upload dist/*')  # Requires ~/.pypirc Keys
+        print("\n*** The Release was PUBLISHED SUCCESSFULLY to PyPI! :) ***\n")
+    else:
+        print("\n>>> The Release was NOT PUBLISHED to PyPI! <<<\n")
+    sys.exit()
+
 setup(
     name='masterqa',
-    version='1.1.7',
+    version='1.2.0',
     description='Automation-Assisted Manual Testing - http://masterqa.com',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    platforms='Mac * Windows * Linux',
+    platforms=["Windows", "Linux", "Mac OS-X"],
     url='http://masterqa.com',
     author='Michael Mintz',
     author_email='mdmintz@gmail.com',
     maintainer='Michael Mintz',
     license='The MIT License',
     install_requires=[
-        'seleniumbase',
+        'seleniumbase>=1.63.9',
         ],
     packages=['masterqa'],
     entry_points={
