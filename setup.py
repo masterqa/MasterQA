f086c53bc0426e2cd7d3e21428151baaf2932ad0
Michael Mintz <mdmintz@gmail.com>
2022-12-07 15:06:31 -0500

    Version 1.7.1

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ if sys.argv[-1] == "publish":
 
 setup(
     name="masterqa",
-    version="1.7.0",
+    version="1.7.1",
     description="Automation-Assisted Manual Testing - https://masterqa.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
