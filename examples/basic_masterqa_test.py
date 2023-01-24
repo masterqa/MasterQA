674c5244e302ee5ad291756f2d9eed56410bd686
Michael Mintz <mdmintz@gmail.com>
2023-01-23 23:47:11 -0500

    Update an example test

--- examples/basic_masterqa_test.py
+++ examples/basic_masterqa_test.py
@@ -6,9 +6,11 @@ class MasterQATests(MasterQA):
         self.open("https://seleniumbase.io/devices/")
         self.highlight("div.mockup-wrapper")
         self.verify("Do you see 4 computer devices?")
+
         self.open("https://seleniumbase.io/demo_page")
         self.highlight('table')
         self.verify("Do you see elements in a table?")
+
         self.open("https://xkcd.com/1700/")
         self.highlight("#comic")
         self.verify("Do you see a webcomic?")
