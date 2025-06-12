a766d4b4187f81542b956c031750f1b40790d271
Michael Mintz <mdmintz@gmail.com>
2025-06-12 18:51:24 -0400

    Version 1.10.1

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ if sys.argv[-1] == "publish":
 
 setup(
     name="masterqa",
-    version="1.10.0",
+    version="1.10.1",
     description="Automation-Assisted Manual Testing - https://masterqa.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
